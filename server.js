e32adc9287ea925c80e10e7bc377df90b81dee34
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-03-18 16:15:29 -0400

    Initialized repository for Note Taker application

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const api = require("./routes/index");
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+
+app.use(express.static("public"));
+
+app.use("/api", api);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
+
